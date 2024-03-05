2b05cca3e6464a70eebe5187ded5633129317866
Frédéric Jacques <gary.bob88@gmail.com>
2024-03-05 12:19:52 -0500

    update packages to fix dependabot vulnerability

---
+++ SuperHeroNG/SuperHero.UI/update-angular-version.ps1
@@ -0,0 +1,12 @@
+Write-Host "A script to update the angular version in the project"
+
+$version = "16"
+
+Write-Host "ng update preview"
+ng update
+
+Write-Host "Update the angular version $version"
+ng update @angular/cdk@$version --force --allow-dirty
+ng update @angular/cli@$version --force --allow-dirty
+ng update @angular/core@$version --force --allow-dirty
+ng update @angular/material@$version --force --allow-dirty
