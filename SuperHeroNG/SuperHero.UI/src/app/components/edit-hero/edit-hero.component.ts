d612614b8eb086fe8f763325cafeee78e9db7dbd
Frédéric Jacques <gary.bob88@gmail.com>
2025-03-15 09:01:20 -0400

    fixes

--- SuperHeroNG/SuperHero.UI/src/app/components/edit-hero/edit-hero.component.ts
+++ SuperHeroNG/SuperHero.UI/src/app/components/edit-hero/edit-hero.component.ts
@@ -1,7 +1,9 @@
 import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { SuperHero } from 'src/app/models/super-hero';
 import { SuperHeroService } from 'src/app/services/super-hero.service';
 
@@ -12,12 +14,13 @@ import { SuperHeroService } from 'src/app/services/super-hero.service';
     imports: [
       NgIf,
       MatFormFieldModule,
+      MatButtonModule,
+      MatInputModule,
       FormsModule
     ]
 })
 export class EditHeroComponent {
   private readonly superHeroService = inject(SuperHeroService);
-
 
   @Input() hero?: SuperHero;
   @Output() heroesUpdated = new EventEmitter<SuperHero[]>();
@@ -31,6 +34,7 @@ export class EditHeroComponent {
   deleteHero(hero: SuperHero) {
     this.superHeroService.deleteHero(hero).subscribe(result => {
       this.heroesUpdated.emit(result);
+      this.hero = undefined;
     })
   }
 
